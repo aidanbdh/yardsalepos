e77a98b18649d3399d33d7806e716fcbd3c2e277
Aidan Harvey <aidanharveydev@sarahs-macbook.attlocal.net>
2017-04-28 10:12:24 -0700

    Finished #3

--- public/display/views/transactions.js
+++ public/display/views/transactions.js
@@ -1,10 +1,12 @@
 const React = require('react')
 const connect = require('react-redux')
 const Table = require('../components/table.js')
+const Totals = require('../components/totals.js')
 
 module.exports = connect()(
   () =>
     <div>
       <Table/>
+      <Totals/>
     </div>
 )
